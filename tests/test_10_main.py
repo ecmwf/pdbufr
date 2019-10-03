36a8d98502bc90f173b157374392505a9ef6d18d
Alessandro Amici <a.amici@bopen.eu>
2019-10-03 21:56:57 +0200

    Add test file for CLI

---
+++ tests/test_10_main.py
@@ -0,0 +1,11 @@
+
+import pytest
+
+from pdbufr import __main__
+
+
+def test_main():
+    __main__.main(argv=['selfcheck'])
+
+    with pytest.raises(RuntimeError):
+        __main__.main(argv=['bad-command'])
