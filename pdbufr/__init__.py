e7dd3afa0c72c1b619f042b818c5b589284ac5ed
Alessandro Amici <a.amici@bopen.eu>
2020-12-09 16:21:48 +0100

    make pandas optional

--- pdbufr/__init__.py
+++ pdbufr/__init__.py
@@ -1,5 +1,12 @@
-from .bufr_read import read_bufr
+from .bufr_structure import stream_bufr
+
+__all__ = ["stream_bufr"]
+
+try:
+    from .bufr_read import read_bufr
+
+    __all__ += ["read_bufr"]
+except ModuleNotFoundError:
+    pass
 
 __version__ = "0.8.3.dev0"
-
-__all__ = ["read_bufr"]
